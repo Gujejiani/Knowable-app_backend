750f493689562e12ef0e205f0a576322325b3bc5
kakha <kakha.gujejiani.1@iliauni.edu.ge>
2024-10-27 11:28:07 +0400

    refactor: Prettier changes added

--- src/events/event-emiter.service.ts
+++ src/events/event-emiter.service.ts
@@ -1,4 +1,4 @@
-// event-emitter.service.ts
+
 import { Injectable } from '@nestjs/common';
 import { EventEmitter2 } from '@nestjs/event-emitter';
 
@@ -6,7 +6,7 @@ import { EventEmitter2 } from '@nestjs/event-emitter';
 export class EventEmitterService {
   constructor(private readonly eventEmitter: EventEmitter2) {}
 
-  // Emit an event to check if a course exists
+
   async checkCourseExist(courseId: number): Promise<boolean> {
     return new Promise<boolean>((resolve) => {
       this.eventEmitter.emit('course.exists.check', courseId, resolve);
